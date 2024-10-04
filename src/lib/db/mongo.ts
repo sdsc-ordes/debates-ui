2cb1955ff30bd3157eac70ff5042e7d9d87e0a5f
sabinem <5292683+sabinem@users.noreply.github.com>
2024-10-04 18:23:26 +0200

    feat: mongodb brings back full subtitles the goal is to take the subtitles from the mongodb and not from the original srt file in the videoplayer

--- src/lib/db/mongo.ts
+++ src/lib/db/mongo.ts
@@ -1,7 +1,8 @@
 import { MongoClient } from "mongodb"
 const mongoUrl = import.meta.env.VITE_MONGO_URL;
+const debatesDB = import.meta.env.VITE_MONGO_DB;
 const client = new MongoClient(mongoUrl)
-const db = client.db("debates");
+const db = client.db(debatesDB);
 
 export function start_mongo(): Promise<MongoClient> {
     console.log("Starting mongo")
--- src/routes/testmongo/+page.server.ts
+++ src/routes/testmongo/+page.server.ts
@@ -1,15 +1,17 @@
 import db from "$lib/db/mongo";
 import type { PageServerLoad } from "./$types";
+const videoCollection = import.meta.env.VITE_MONGO_VIDEO_COLLECTION;
 
 export const load: PageServerLoad = async () => {
   try {
-    const data = await db.collection("segments").find({}).toArray();
-    const serializedData = data.map((segment) => ({
-      ...segment,
-      _id: segment._id.toString(),
+    const data = await db.collection(videoCollection).find({}).toArray();
+    const serializedData = data.map((video) => ({
+      ...video,
+      _id: video._id.toString(),
     }));
+    console.log(serializedData);
     return {
-      segments: serializedData
+      video: serializedData
     }
   } catch (error) {
     console.error('Error loading collections:', error);
