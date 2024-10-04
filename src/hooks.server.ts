1e067b95b44b3cf66eae6eea18bd9047387da478
sabinem <5292683+sabinem@users.noreply.github.com>
2024-10-04 07:55:47 +0200

    remove log statements from the server hook

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,7 +1,5 @@
-console.log("Importing Mongo module");
 import { start_mongo } from "./lib/db/mongo";
 
-console.log("In the hook");
 start_mongo()
   .then((): void => {
     console.log("Mongo started");
@@ -9,7 +7,3 @@ start_mongo()
   .catch((e) => {
     console.error("Mongo connection error:", e);
   });
-
-console.log("Hook script complete");
-const mongoUrl = import.meta.env.VITE_MONGO_URL;
-console.log(mongoUrl);
