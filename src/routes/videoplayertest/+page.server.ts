1c3462a4ae7981bc70331e482f481e60923f3901
sabinem <5292683+sabinem@users.noreply.github.com>
2024-10-11 08:45:30 +0200

    refactor: add mongo server also to test page

---
+++ src/routes/videoplayertest/+page.server.ts
@@ -0,0 +1,18 @@
+import db from "$lib/mongo/mongo";
+import type { PageServerLoad } from "./$types";
+const videoCollection = import.meta.env.VITE_MONGO_VIDEO_COLLECTION;
+
+export const load: PageServerLoad = async () => {
+  try {
+    const data = await db.collection(videoCollection).find({}).toArray();
+    const serializedData = data.map((video) => ({
+      ...video,
+      _id: video._id.toString(),
+    }));
+    return {
+      video: serializedData
+    }
+  } catch (error) {
+    console.error('Error loading collections:', error);
+  }
+};
