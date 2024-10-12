a6e98aa4434a1f5a1933afccab3c5c6aaac5a37e
sabinem <5292683+sabinem@users.noreply.github.com>
2024-10-12 07:29:48 +0200

    feat(videoplayertest): enable editing for subtitles

--- src/lib/components/SegmentDisplay.svelte
+++ src/lib/components/SegmentDisplay.svelte
@@ -2,13 +2,35 @@
     import type { Subtitle, TimeUpdateParameters } from "$lib/interfaces/videoplayer.interface";
     export let subtitles: Subtitle[] = [];
     export let timeUpdateParameters: TimeUpdateParameters;
+    let editSubtitles: boolean = false;
+    function updateSubtitle(index: number, updatedText: string) {
+      subtitles[index].content =  updatedText;
+    }
+    function toggleEditSubtitles() {
+      editSubtitles = !editSubtitles;
+    }        
 </script>
+<button on:click={toggleEditSubtitles}>
+  {editSubtitles ? 'Disable Subtitle Editing' : 'Enable Subtitle Editing'}
+</button>
 
 <p>
     {#each subtitles as subtitle, index}  
       {#if subtitle.segment_nr === timeUpdateParameters.currentSegmentIndex}
       <span class={index === timeUpdateParameters.currentSubtitleIndex - 1 ? 'highlighted' : ''}>
-        {subtitle.content}
+        {#if editSubtitles}
+        <div>
+          <textarea
+            id={`subtitle-${index}`}
+            bind:value={subtitle.content}
+            on:input={(e) =>
+              updateSubtitle(index, e.target.value)}
+            class="editable-textarea"
+          />
+        </div>
+        {:else}
+          {subtitle.content}
+        {/if}
       </span>    
       {/if}  
     {/each}
@@ -18,4 +40,8 @@
     .highlighted {
         color:red;
     }  
+    .editable-textarea {
+      width: 1000px;
+      height: 50px;
+    }
 </style>
