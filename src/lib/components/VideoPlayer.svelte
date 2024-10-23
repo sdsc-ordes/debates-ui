44b032eba40ac1a2018ae8e9ef867d533ded9295
sabinem <5292683+sabinem@users.noreply.github.com>
2024-10-23 19:44:20 +0200

    fix: readd videoplayer component under correct name

---
+++ src/lib/components/VideoPlayer.svelte
@@ -0,0 +1,60 @@
+<script lang="ts">
+    import { getMediaSources } from "$lib/s3/s3";
+    import { onMount } from "svelte";
+    import { onVideoTimeUpdate } from "$lib/utils/videoTimeUpdate";
+    import { jumpToTime } from "$lib/utils/videoStartUtils";
+    import type { TimeUpdateParameters } from "$lib/interfaces/videoplayer.interface";
+    import type { Subtitle, Segment, Speaker } from "$lib/interfaces/videoplayer.interface";
+
+    export let videoId: string;
+    export let startTime: number;
+    export let timeUpdateParameters: TimeUpdateParameters;
+    export let subtitles: Subtitle[] = [];
+    export let segments: Segment[] = [];
+    export let speakers: Speaker[] = [];
+    export let video: HTMLVideoElement;
+    let { videoSrc, trackSrc } = getMediaSources(videoId);
+
+    function handleTimeUpdate() {
+      timeUpdateParameters = onVideoTimeUpdate(
+        video.currentTime,
+        subtitles,
+        segments,
+        speakers,
+        timeUpdateParameters.currentSubtitleIndex,
+      );
+    }  
+
+    onMount(async () => {
+      if (startTime) {
+        jumpToTime(video, startTime);
+      }
+    });      
+  </script>
+  
+  <div class="video-container">
+    <video
+      class="video"
+      bind:this={video}
+      on:timeupdate={handleTimeUpdate}
+      controls
+      autoplay
+    >
+      <source src={videoSrc} type="video/mp4" />
+      <track
+        src={trackSrc}
+        kind="captions"
+        srclang="en"
+        label="english_captions"
+      />
+      Your browser does not support the video tag.
+    </video>
+  </div>
+  
+  <style>
+    .video {
+      width: 600px;
+      border: 1px solid #ddd;
+    }
+  </style>
+  
