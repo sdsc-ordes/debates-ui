b7c86a9bc6291b586e80c270599fc2e63e85704c
sabinem <5292683+sabinem@users.noreply.github.com>
2024-11-01 13:30:59 +0100

    fix: add component SearchResultContainer
    part of make components for search page

---
+++ src/lib/components/SearchResultContainer.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+    import FacetCounts from "$lib/components/FacetCounts.svelte";
+    import StatementList from "$lib/components/StatementList.svelte";
+    export let searchResult;
+  </script>
+  <div class="container">
+    <FacetCounts facetCounts={searchResult.facet_counts} />
+    <StatementList docs={searchResult.response?.docs} query={searchResult.responseHeader.params.q} highlighting={searchResult.highlighting} />
+  </div>
