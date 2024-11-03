43621ef357b3ed0600adb844b8616b0776f7fdeb
sabinem <5292683+sabinem@users.noreply.github.com>
2024-11-03 08:44:38 +0100

    feat: pass through solrQuery and onSearch to facet counts

--- src/lib/components/SearchResultContainer.svelte
+++ src/lib/components/SearchResultContainer.svelte
@@ -1,9 +1,18 @@
 <script lang="ts">
-    import FacetCounts from "$lib/components/FacetCounts.svelte";
-    import StatementList from "$lib/components/StatementList.svelte";
-    export let searchResult;
-  </script>
-  <div class="container">
-    <FacetCounts facetCounts={searchResult.facet_counts} />
-    <StatementList docs={searchResult.response?.docs} query={searchResult.responseHeader.params.q} highlighting={searchResult.highlighting} />
-  </div>
+  import FacetCounts from "$lib/components/FacetCounts.svelte";
+  import StatementList from "$lib/components/StatementList.svelte";
+  import type { SolrQuery, SolrResponse } from "$lib/interfaces/solr.interface";
+
+  export let searchResult: SolrResponse;
+  export let solrQuery: SolrQuery;
+  export let onSearch: (solrQuery: SolrQuery) => void;
+</script>
+
+<div class="container">
+  <FacetCounts {solrQuery} {onSearch} {searchResult} />
+  <StatementList
+    docs={searchResult.response?.docs}
+    query={searchResult.responseHeader.params.q}
+    highlighting={searchResult.highlighting}
+  />
+</div>
