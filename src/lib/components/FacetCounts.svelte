ff006070f524b42a7cd99433a6cefd9519bf428f
sabinem <5292683+sabinem@users.noreply.github.com>
2024-11-03 09:07:11 +0100

    refactor facet count component: mark active, allow to trigger search

--- src/lib/components/FacetCounts.svelte
+++ src/lib/components/FacetCounts.svelte
@@ -1,5 +1,22 @@
 <script lang="ts">
-  export let facetCounts;
+  import type { SolrQuery } from "$lib/interfaces/solr.interface";
+  import { displayIsoDate } from "$lib/utils/displayUtils";
+  export let searchResult;
+  export let solrQuery: SolrQuery;
+  export let onSearch: (solrQuery: SolrQuery) => void; // Function to trigger search
+  let facetCounts = searchResult.facet_counts;
+  console.log(facetCounts);
+
+  const displayFunctions: { [key: string]: (label: string) => string } = {
+    debate_schedule: (label) => displayIsoDate(label)
+  };
+
+  // Default display function if no specific function is found for a facet field
+  const defaultDisplayFunction = (label: string) => label;
+
+  function getDisplayFunction(fieldKey: string): (label: string) => string {
+    return displayFunctions[fieldKey] || defaultDisplayFunction;
+  }
 
   // Helper function to get facet values as key-value pairs
   const getFacetFields = (facetFields) => {
@@ -14,36 +31,39 @@
     return fields;
   };
 
-  // Helper function to format facet field titles
-  const formatTitle = (title) => {
-    return title
-      .replace(/_/g, " ")
-      .replace(/\b\w/g, (char) => char.toUpperCase());
-  };
-
   // Function to handle facet clicks
-  function handleFacetClick(facetLabel: string) {
-    console.log(`Facet clicked: ${facetLabel}`);
-    // Call your Solr query function here, passing the clicked facet label
-    // fetchSolrData(facetLabel); // Uncomment and replace with your Solr function
-
-    alert(`clicked with ${facetLabel}`);
+  function handleFacetClick(facetField: string, facetValue: string) {
+    // Update solrQuery with selected facet
+    solrQuery.facetField = facetField;
+    solrQuery.facetValue = facetValue;
+    // Trigger search with updated solrQuery
+    onSearch(solrQuery);
+  }
+  function isActive(facetField: string, facetLabel: string): boolean {
+    return (
+      facetField === solrQuery.facetField && facetLabel === solrQuery.facetValue
+    );
   }
 </script>
 
-{#if facetCounts}
+{#if searchResult.facet_counts}
   <div class="facets">
-    {#each getFacetFields(facetCounts.facet_fields) as field}
-      <h4 class="facet-title">{formatTitle(field.key)}</h4>
+    {#each getFacetFields(searchResult.facet_counts.facet_fields) as field}
+      <h4 class="facet-title">{field.key}</h4>
       {#each field.facets as facet}
         {#if facet.count}
-          <div 
-            class="facet-item" 
-            on:click={() => handleFacetClick(facet.label)}
-            style="cursor: pointer;"
+          <button
+            class="facet-item {isActive(field.key, facet.label)
+              ? 'active'
+              : ''}"
+            on:click={() => handleFacetClick(field.key, facet.label)}
           >
-            {facet.label}: <span>{facet.count}</span>
-          </div>
+            {getDisplayFunction(field.key)(facet.label)}
+          </button>
+          <button class="facet-item" on:click={() => handleFacetClick("", "")}>
+            <span>x</span>
+          </button>
+          : <span>{facet.count}</span>
         {/if}
       {/each}
     {/each}
@@ -76,5 +96,11 @@
   .facet-item span {
     font-weight: bold;
     margin-left: 0.5rem;
+  }
+
+  button.active {
+    font-weight: bold;
+    text-decoration: underline;
+    background: lightblue;
   }
 </style>
