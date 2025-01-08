180ad11c3efbbc02f6e5a225bf36c25306dbc0d9
Hannah Casey <61227037+hanaCasey@users.noreply.github.com>
2025-01-08 11:43:53 +0100

    style: style search bar

--- src/lib/components/SearchForm.svelte
+++ src/lib/components/SearchForm.svelte
@@ -15,46 +15,55 @@
     }
 </script>
 
-<form on:submit|preventDefault={submitForm}>
+<form on:submit|preventDefault={submitForm} class="search-form">
     <input
-        id="#query-input"
+        class="search-input"
         type="text"
         bind:value={solrQuery.queryTerm}
         placeholder="Enter search term"
     />
-    <button class="search-button" type="submit">Search</button>
-    <button class="option-button" type="button" on:click={resetForm}>Reset</button>
+    <button class="option-button" type="button" on:click={resetForm}>
+        <i class="fa fa-xmark"></i>
+    </button>
+    <button class="button-primary" type="submit">
+        <i class="fa fa-search"></i> Search
+    </button>
 </form>
 
 <style>
-    #query-input {
-        flex: 1;
-        padding: 0.5rem;
-        font-size: 1rem;
-        border: 1px solid #ccc;
-        border-radius: 4px 0 0 4px;
+    .search-form {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        gap: 1rem;
+        width: 100%;
+        max-width: 40rem;
+        margin: 0 auto;
+        padding: 10px;
+        padding-right: 20px;
+        border-radius: 50px;
+        box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.1);
+        /* background-color: var(--background-color); */
     }
-
-    .search-button {
-        padding: 0.5rem 1rem;
-        font-size: 1rem;
-        font-weight: bold;
-        color: #fff;
-        background-color: #ff3e00;
+    .search-input {
+        flex: 1;
+        padding: 10px 15px;
         border: none;
-        border-radius: 0 4px 4px 0;
-        cursor: pointer;
-        transition: background-color 0.3s;
+        border-radius: 50px;
+        font-size: 12px;
+        color: var(--text-color);
+        outline: none;
+        transition: border-color 0.3s ease;
     }
 
     .option-button {
-        margin-top: 0.5rem;
-        font-size: 0.9rem;
-        color: #ff3e00;
+        /* margin-top: 0.5rem;
+        font-size: 0.9rem; */
+        color: gray;
         background: none;
         border: none;
         cursor: pointer;
         text-align: left;
-        margin-left: 2rem;
+        /* margin-left: 2rem; */
     }
 </style>
