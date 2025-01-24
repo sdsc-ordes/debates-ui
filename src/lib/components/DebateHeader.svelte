100dcdcddc7fe0cd3d53e6e475d0cac48d0c2662
Hannah Casey <61227037+hanaCasey@users.noreply.github.com>
2025-01-24 15:46:06 +0100

    fix(DebateHeader): minor fixing of layout

--- src/lib/components/DebateHeader.svelte
+++ src/lib/components/DebateHeader.svelte
@@ -1,14 +1,15 @@
 <script lang="ts">
-    import type { Debate } from "$lib/interfaces/backend.interface";
-    import {
-        displayIsoDate,
-    } from "$lib/utils/displayUtils";
+  import type { Debate } from "$lib/interfaces/backend.interface";
+  import { displayIsoDate } from "$lib/utils/displayUtils";
 
-    export let debate: Debate;
+  export let debate: Debate;
+</script>
 
-  </script>
-
-  <h3>{debate.type } {debate.session }
-    <i class="fa fa-calendar" aria-hidden="true"></i>
-    {displayIsoDate(debate.schedule)}
-  </h3>
+<div class="card-title-large" style="color: var(--primary-dark-color);">
+  {debate.type}
+  {debate.session}
+  <div class="date-time-item">
+    <!-- <i class="fa fa-calendar" aria-hidden="true"></i> -->
+    <div class="card-title-small">{displayIsoDate(debate.schedule)}</div>
+  </div>
+</div>
