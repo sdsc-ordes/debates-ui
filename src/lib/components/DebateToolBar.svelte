50caf92e0024c51816fcd7c12f596e67e4ef32d5
Hannah Casey <61227037+hanaCasey@users.noreply.github.com>
2025-01-24 15:45:11 +0100

    feat(Toolbar): toolbar with download button

---
+++ src/lib/components/DebateToolBar.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+    import FileDownload from "./FileDownload.svelte";
+    import type { SignedUrl } from "$lib/interfaces/backend.interface";
+
+    export let downloadUrls: SignedUrl[] = [];
+</script>
+
+<div class="toolbar">
+    <div><FileDownload {downloadUrls} /></div>
+</div>
+
+<style>
+    .toolbar {
+        background-color:var(--on-primary);
+        width: 100%;
+        height: 2rem;
+        border-radius: 2rem;
+        display: flex;
+        align-items: center;
+        justify-content: flex-end; 
+        padding: 5px; 
+        margin: 0; 
+        box-sizing: border-box; 
+    }
+</style>
